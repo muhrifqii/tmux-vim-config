5e6fb6a7bcb8ae897c5938d31de7515fb154f022
muhrifqii <muh_rif@live.com>
2017-01-14 13:32:57 +0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# this script did not prepare tmux nor vim installation helper (not yet maybe). Instead, it's just simplify copy paste the configuration file to the home directory. Make sure you make this script executable first.
+#
+
+cp .tmux.conf ~/
+cp .vimrc.conf ~/
+
