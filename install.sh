c7929719b1fd1a60137cd9f830b170f8fefdde79
Muhammad Rifqi Fatchurrahman Putra Danar <muhrifqii@users.noreply.github.com>
2017-01-26 13:21:25 +0700

    add auto clone tpm

--- install.sh
+++ install.sh
@@ -11,3 +11,6 @@ cp .vimrc.conf ~/
 echo "downloading vundle, a vim plugin manager"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "downloading tpm, a tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
